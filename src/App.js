74c44f75076e571c6a537798bdad61e2e66cdd1e
liu_hw <liu_hw@neusoft.com>
2023-10-10 09:01:56 +0800

    init;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// Import React dependencies.
+import React, { useState } from "react";
+// Import the Slate editor factory.
+import { createEditor } from "slate";
 
-function App() {
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from "slate-react";
+
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
+
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Add the editable component inside the context.
+    <Slate editor={editor} initialValue={initialValue}>
+      <Editable />
+    </Slate>
   );
-}
+};
 
 export default App;
